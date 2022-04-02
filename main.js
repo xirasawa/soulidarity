09e6766d5da0f492ec59f286f1f2359dce3f67a4
xirasawa <mathshiro@gmail.com>
2022-04-01 21:03:58 -0300

    commit inicial do projeto

---
+++ main.js
@@ -0,0 +1,121 @@
+/* abre e fecha o menu qunado clicar no icone: hamburguer e X */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da pag qnd der o scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/* ONG carosel slider swiper */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  /* mousewheel: true, */
+  keyboard: true
+})
+
+/* scrollreveal: mostrar elementos quando der scroll na pagina */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 600,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .swiper,
+  #donations header, #donations .donations,
+  #contact .text, #contact .links,
+  #footer .brand, #footer .social
+  `,
+  { interval: 100 }
+)
+
+/* button back to top */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
+
+/* SLIDER DONATIONS */
+$('.js-range-slider').ionRangeSlider({
+  type: 'single',
+  from: 5,
+  grid: true,
+  skin: 'round',
+  values: ['R$10', 'R$25', 'R$50', 'R$100', 'R$250', 'Outro valor'],
+  onChange: function (data) {
+    document.querySelector('.qrcode.show')?.classList.remove('show')
+    document.querySelector('#qrcode-' + data.from_pretty).classList.add('show')
+  },
+  onStart: function (data) {
+    document.querySelector('#qrcode-' + data.from_pretty).classList.add('show')
+  }
+})
