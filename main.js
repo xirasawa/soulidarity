73c6585f185672f2be9db5169ffb983838e717a2
xirasawa <mathshiro@gmail.com>
2022-04-05 03:59:41 -0300

    link sptv

--- main.js
+++ main.js
@@ -110,7 +110,7 @@ $('.js-range-slider').ionRangeSlider({
   from: 3,
   grid: true,
   skin: 'round',
-  values: ['R$10', 'R$25', 'R$50', 'R$150', 'R$250', 'outro<br>valor'],
+  values: ['R$10', 'R$25', 'R$50', 'R$150', 'R$250', 'outro valor'],
   onChange: function (data) {
     document.querySelector('.qrcode.show')?.classList.remove('show')
     document.querySelector('#qrcode-' + data.from_pretty).classList.add('show')
